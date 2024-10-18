30c115229f082aa167d8d3ef94928eea1a1422cd
Celso <celso.sjunior@outlook.com>
2024-10-18 09:02:04 -0300

    REST listening in port 3000

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(request: Request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/users') {
+
+      if (request.method === 'GET') {
+        
+        const users: User[] = [];
+
+        return Response.json ({
+          users
+        })
+      
+      } 
+    }
+
+    return new Response('Not found', { status: 404 });
+    
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
